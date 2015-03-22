1faa7d38a30159ed212fd319ae60cd26f6f9e2a9
masterUNG <phrombutr@gmail.com>
2015-03-22 09:58:38 +0700

    Toast

--- app/src/main/java/appewtc/masterung/ishihara21mar15/MainActivity.java
+++ app/src/main/java/appewtc/masterung/ishihara21mar15/MainActivity.java
@@ -1,18 +1,114 @@
 package appewtc.masterung.ishihara21mar15;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    //Explicit
+    private TextView txtQuestion;
+    private ImageView imvIshihara;
+    private RadioGroup ragChoice;
+    private RadioButton radChoice1, radChoice2, radChoice3, radChoice4;
+    private Button btnAnswer;
+    private int intRadio;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        //Initial Widget
+        initialWidget();
+
+        //Create Button Controller
+        buttonController();
+
+        //Create Radio Controller
+        radioController();
+
+    }   // onCreate
+
+    private void radioController() {
+        ragChoice.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                MediaPlayer objMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_shut);
+                objMediaPlayer.start();
+
+                //setup intRadio
+                switch (i) {
+                    case R.id.radioButton:
+                        intRadio = 1;
+                        break;
+                    case R.id.radioButton2:
+                        intRadio = 2;
+                        break;
+                    case R.id.radioButton3:
+                        intRadio = 3;
+                        break;
+                    case R.id.radioButton4:
+                        intRadio = 4;
+                        break;
+                    default:
+                        intRadio = 0;
+                        break;
+                }   // switch
+
+            }   // event
+        });
+    }   // radioController
+
+    private void buttonController() {
+        btnAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer objMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_long);
+                objMediaPlayer.start();
+
+                //Check Answer
+                checkAnswer();
+
+
+            }   // event
+        });
+    }   // buttonController
+
+    private void checkAnswer() {
+        if (intRadio == 0) {
+            Toast.makeText(MainActivity.this, "กรุณาตอบคำถาม นะคะ", 5000).show();
+        } else {
+
+            
+
+        }
+    }   // checkAnswer
+
+    private void initialWidget() {
+        txtQuestion = (TextView) findViewById(R.id.textView2);
+        imvIshihara = (ImageView) findViewById(R.id.imageView);
+        ragChoice = (RadioGroup) findViewById(R.id.ragChoice);
+        radChoice1 = (RadioButton) findViewById(R.id.radioButton);
+        radChoice2 = (RadioButton) findViewById(R.id.radioButton2);
+        radChoice3 = (RadioButton) findViewById(R.id.radioButton3);
+        radChoice4 = (RadioButton) findViewById(R.id.radioButton4);
+        btnAnswer = (Button) findViewById(R.id.button);
+    }   // initialWidget
 
 
     @Override
@@ -36,4 +132,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}   // Main Class
